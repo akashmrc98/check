5948d39774a62c045b8d82b09393d2751171a873
akashmadduru <akashmadduru@gmail.com>
2023-09-01 19:34:36 +0530

    update

--- src/data/aiSectionData.jsx
+++ src/data/aiSectionData.jsx
@@ -13,14 +13,14 @@ export const aiSectionData = [
 
 import LIVE from "/public/ai_section/1.jpg";
 import KNOWLEDGE from "/public/ai_section/2.jpg";
-import AI from "/public/ai_section/4.jpg";
-import HUB from "/public/ai_section/5.jpg";
-import ACC from "/public/ai_section/7.jpg";
-import IMG from "/public/ai_section/8.jpg";
-import AI_ from "/public/ai_section/9.jpg";
-import HARDWARE from "/public/ai_section/11.jpg";
+import AI from "/public/ai_section/3.jpg";
+import HUB from "/public/ai_section/4.jpg";
+import ACC from "/public/ai_section/5.jpg";
+import IMG from "/public/ai_section/6.jpg";
+import AI_ from "/public/ai_section/7.jpg";
+import HARDWARE from "/public/ai_section/8.jpg";
 import END_POINTS from "/public/ai_section/10.jpg";
-import RISK from "/public/ai_section/12.jpg";
+import RISK from "/public/ai_section/9.jpg";
 
 export const aiImages = [
   LIVE,
